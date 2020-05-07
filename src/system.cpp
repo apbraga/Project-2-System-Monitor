cc8ab88acb75f39225e953524a7b3c22644d92b1
apbraga <alexbraga101@gmail.com>
2020-05-07 20:13:35 -0300

    Complete system implementation

--- src/system.cpp
+++ src/system.cpp
@@ -14,10 +14,10 @@ using std::size_t;
 using std::string;
 using std::vector;
 
-// TODO: Return the system's CPU
+//Return the system's CPU
 Processor& System::Cpu() { return cpu_; }
 
-// TODO: Return a container composed of the system's processes
+//Return a container composed of the system's processes
 vector<Process>& System::Processes() { 
     processes_ = {};
     std::vector<int> pids = LinuxParser::Pids();
@@ -29,25 +29,24 @@ vector<Process>& System::Processes() {
     std::sort(processes_.begin(), processes_.end(),[](Process& p1,Process& p2){
         return (p1.CpuUtilization() < p2.CpuUtilization());
     });
+    return processes_; 
+}
 
 
-    return processes_; }
-
-
-// Test OK: Return the system's kernel identifier (string)
+//Return the system's kernel identifier (string)
 std::string System::Kernel() { return LinuxParser::Kernel(); }
 
-// Not Tested: Return the system's memory utilization
+//Return the system's memory utilization
 float System::MemoryUtilization() { return LinuxParser::MemoryUtilization(); }
 
-// Test OK: Return the operating system name
+//Return the operating system name
 std::string System::OperatingSystem() { return LinuxParser::OperatingSystem(); }
 
-// Not Tested: Return the number of processes actively running on the system
+//Return the number of processes actively running on the system
 int System::RunningProcesses() { return LinuxParser::RunningProcesses(); }
 
-// Not Tested: Return the total number of processes on the system
+//Return the total number of processes on the system
 int System::TotalProcesses() { return LinuxParser::TotalProcesses(); }
 
-// Not Tested: Return the number of seconds since the system started running
+//Return the number of seconds since the system started running
 long int System::UpTime() { return LinuxParser::UpTime(); }
