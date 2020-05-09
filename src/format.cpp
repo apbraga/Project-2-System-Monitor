fd5b5117a4f37f2e4f631af9cacccc2e620c3d5a
Alex Braga <alexbraga101@gmail.com>
2020-05-09 17:37:04 -0300

    fix issue of seconds with 3 digits

--- src/format.cpp
+++ src/format.cpp
@@ -1,22 +1,20 @@
 #include <string>
 #include <cmath>
 #include "format.h"
+#include <sstream>
 
 using std::string;
 
-// Not Tested: Complete this helper function
+// Complete this helper function
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
 // REMOVE: [[maybe_unused]] once you define the function
 string Format::ElapsedTime(long seconds) {
-    //get number of hours from totoal number of seconds
-    long hour = std::floor(seconds/3600);
-    seconds -= hour*3600;
-    // get number of minutes from total number os seconds
-    long minutes = std::floor(seconds/60);
-    //get number of seconds
-    seconds -= minutes*60;
-    //convert to HH:MM:SS formating
-    std::string time = std::to_string(hour) + ":" + std::to_string(minutes) + ":" + std::to_string(seconds);   
-    
-    return time; }
+  int hours, minutes;
+  minutes = seconds / 60;
+  hours = minutes / 60;
+  std::stringstream s;
+  s << hours << ":" << int(minutes % 60) << ":" << int(seconds % 60) << std::endl;
+  return s.str();
+}
+  
